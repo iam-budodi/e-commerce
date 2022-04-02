66c259f9124b8dbbcce93d45159b0d0fd00dd591
Japhet Sebastian <japhet.sebastian01@gmail.com>
2022-04-02 16:37:15 +0300

    configure RESTful app entry point

---
+++ src/main/java/com/commerce/rest/Books.java
@@ -0,0 +1,14 @@
+package com.commerce.rest;
+
+import java.util.ArrayList;
+
+import com.commerce.model.Book;
+
+public class Books extends ArrayList<Book> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
