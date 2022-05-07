fc163f0d4034be88efeeabbd8988fa876ffff6d3
Japhet Sebastian <japhet.sebastian01@gmail.com>
2022-05-07 15:55:04 +0300

    servlet handling http request and response for invoices

--- invoice/src/main/java/com/commerce/invoice/web/InvoiceServlet.java
+++ invoice/src/main/java/com/commerce/invoice/web/InvoiceServlet.java
@@ -1,12 +1,17 @@
 package com.commerce.invoice.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.commerce.invoice.model.Invoice;
+import com.commerce.invoice.service.InvoiceService;
 
 /**
  * Servlet implementation class InvoiceServlet
@@ -15,25 +20,36 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "InvoiceServlet", urlPatterns = { "/invoice" })
 public class InvoiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public InvoiceServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	
+	// ======================================
+    // =          Injection Point           =
+    // ======================================
+	
+	@Inject
+	private InvoiceService invoiceService;
+	 
+	// =======================================
+    // =          Business Methods           =
+    // =======================================
+ 
 	protected void doGet(HttpServletRequest request,
 					HttpServletResponse response)
 					throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ")
-						.append(request.getContextPath());
+		
+		// Handle request
+		Long id = Long.valueOf(request.getParameter("id"));
+
+		// Invoke business logic
+		Invoice invoice = invoiceService.findById(id);
+
+		// Set response content type
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		if (invoice == null)
+			out.print("No invoice with such an id");
+		else
+			out.print(invoice);
 	}
 
 }
